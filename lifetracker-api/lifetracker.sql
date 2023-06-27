00e718700ad7f76aa9cd7ed58e93a64606311670
Lucia Barboza <100799812+luciabarboza@users.noreply.github.com>
2023-06-27 15:57:48 -0700

    updated README file

--- lifetracker-api/lifetracker.sql
+++ lifetracker-api/lifetracker.sql
@@ -1,11 +1,18 @@
+-- step 1 Let the user know that they're about to delete the `lifetracker` database and prompt them to confirm that is what they want.
+
 \echo 'You are about to delete the lifetracker database. Would you like to delete and recreate lifetracker db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
 
+-- step 2 Drop the `lifetracker` database and then create a new `lifetracker` database, before connecting to the `lifetracker` database.
 DROP DATABASE lifetracker;
 CREATE DATABASE lifetracker;
 \connect lifetracker
 
+-- step 3 Run the `lifetracker-schema.sql` file.
+
 \i lifetracker-schema.sql
+
+-- follow the same steps but with test database
 
 \echo 'Delete and recreate lifetracker_test db?'
 \prompt 'Return for yes or control-C to cancel > ' foo
